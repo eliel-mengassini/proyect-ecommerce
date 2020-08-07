dbec2fe23cbcc5ba56828fe155146883f16b62e6
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-08-07 14:57:48 -0300

    update 4

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
+function activateButton() {
+    if (document.getElementById("inputEmail").value!="" &&
+        document.getElementById("inputPassword").value!=""){
+        document.getElementById("button").innerHTML= disabled=false;
+    } else {
+
+    }
+}
+
 function redirect() {
-    location.replace("https://eliel-mengassini.github.io/proyect-ecommerce/home.html")
+    location.replace("https://eliel-mengassini.github.io/proyect-ecommerce/home.html");
 }
