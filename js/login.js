1c2caf50305b6a3bd7c843c2cb7b8b2173aa121c
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-08-19 14:49:55 -0300

    update 9.7

--- js/login.js
+++ js/login.js
@@ -1,54 +1,38 @@
 // en este array voy a guardar lo que se encuentre en el json
 var usersArray = [];
+let inputName = document.getElementById("inputUser");
+let inputPassword = document.getElementById("inputPassword");
+
 
 //esta es la funcion que verificara que los datos que dio el ususario, se enc
-function validateUser(array, userIn, passwordIn) { 
-    for (let i = 0; i < array.length; i++) {
-        let usuario = array[i];
-        if (usuario.email == userIn && usuario.password == passwordIn){
-            return true;
-        }
-    }
-
-    return false;
-} 
+function validateUser(UsersArray, inputName, inputPassword) {
+    if (inputName == Usuarios.name && inputPassword == UsersArray.password){
+        return true;
+     }
+     else { 
+         return false;
+     }
+}
 
 
+document.addEventListener("DOMContentLoaded", function (e) {
 
-document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("submitBtn").addEventListener("click", function (e) {
 
-    document.getElementById("submitBtn").addEventListener("click", function(e) {
-
-        let inputEmail = document.getElementById("inputUser");
-        let inputPassword = document.getElementById("inputPassword");
-        let camposCompletos = true;
+        getJSONData(USUARIOS_URL).then(function (resultObj) {
+            if (resultObj.status === "ok") {
+                usersArray = resultObj.data;
+            }
+        });
         
-        if (inputEmail.value === '') {
-            inputEmail.classList.add("invalid");
-            camposCompletos = false;
+        if (validateUser(usersArray, inputName.value, inputPassword.value)) {
+            window.location = "https://eliel-mengassini.github.io/proyect-ecommerce/home.html";
+        } else {
+            alert("Usuario o contraseña incorrectas!");
         }
 
-        if (inputPassword.value === ''){
-            inputPassword.classList.add("invalid");
-            camposCompletos = false;
-        }
 
-        if (camposCompletos) {
 
-            getJSONData(USUARIOS_URL).then(function (resultObj) {
-                if (resultObj.status === "ok") {
-                    usersArray = resultObj.data;
-        
-                    if ( validateUser(usersArray, inputEmail.value, inputPassword.value) ){
-                        window.location = "https://eliel-mengassini.github.io/proyect-ecommerce/home.html";
-                    }else{
-                        alert("Usuario o contraseña incorrectas!");
-                    }
-                }
-            });
 
-        }else{
-            alert("Debes ingresar los datos!")
-        }
     });
 });
