0064bee5a4a39694c6391c3ae082535b087ae31d
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-08-21 10:54:30 -0300

    update11

--- js/login.js
+++ js/login.js
@@ -1,59 +1,4 @@
-
 const USUARIOS_URL = "https://eliel-mengassini.github.io/proyect-ecommerce/js/usuarios.json";
-
-var usersArray = [];
-
-function validateUser(usersArray, userIn, passwordIn) { 
-    for (let i = 0; i < usersArray.length; i++) {
-        let usuario = usersArray[i];
-        if (usuario.email == userIn && usuario.password == passwordIn){
-            return true;
-        }
-    }
-
-    return false;
-} 
-
-
-
-document.addEventListener("DOMContentLoaded", function(e){
-
-    document.getElementById("submitBtn").addEventListener("click", function(e) {
-
-        let inputEmail = document.getElementById("inputUser");
-        let inputPassword = document.getElementById("inputPassword");
-        let camposCompletos = true;
-        
-        if (inputEmail.value === "") {
-            inputEmail.classList.add("invalid");
-            camposCompletos = false;
-        }
-
-        if (inputPassword.value === ""){
-            inputPassword.classList.add("invalid");
-            camposCompletos = false;
-        }
-
-        if (camposCompletos) {
-
-            getJSONData2(USUARIOS_URL).then(function (resultObj) {
-                if (resultObj.status === "ok") {
-                    usersArray = resultObj.data;
-        
-                    if ( validateUser(usersArray, inputEmail.value, inputPassword.value) ){
-                        window.location.replace("https://eliel-mengassini.github.io/proyect-ecommerce/home.html");
-                    }else{
-                        alert("Usuario o contraseña incorrectas!");
-                    }
-                }
-            });
-
-        }else{
-            alert("Debes ingresar los datos!")
-        }
-    });
-});
-
 
 var getJSONData2 = function(url){
     var result = {};
@@ -76,3 +21,56 @@ var getJSONData2 = function(url){
         return result;
     });
 }
+
+var usersArray = [];
+
+function validateUser(usersArray, userIn, passwordIn) { 
+    for (let i = 0; i < usersArray.length; i++) {
+        let usuario = usersArray[i];
+        if (usuario.name == userIn && usuario.password == passwordIn){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+} 
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+    document.getElementById("submitBtn").addEventListener("click", function(e) {
+
+        let inputUser = document.getElementById("inputUser");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+        
+        if (inputUser.value === "") {
+            inputUser.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (inputPassword.value === ""){
+            inputPassword.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (camposCompletos) {
+
+            getJSONData2(USUARIOS_URL).then(function (resultObj) {
+                if (resultObj.status === "ok") {
+                    usersArray = resultObj.data;
+        
+                    if ( validateUser(usersArray, inputUser.value, inputPassword.value) ){
+                        window.location = "https://eliel-mengassini.github.io/proyect-ecommerce/home.html";
+                    }else{
+                        alert("Usuario o contraseña incorrectas!");
+                    }
+                }
+            });
+
+        }else{
+            alert("Debes ingresar los datos!")
+        }
+    });
+});
