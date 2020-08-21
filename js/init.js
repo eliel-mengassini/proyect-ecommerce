8f069573606228d3cdd8e7e9a366d1726f5401f9
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-08-21 12:01:13 -0300

    update11

--- js/init.js
+++ js/init.js
@@ -8,41 +8,51 @@ const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  
+  let userlogged = localStorage.getItem("user-logged")
+  let infouser = document.getElementById("info-user");
+  let user = document.getElementById("user");
+
+  if (userlogged) {
+    userlogged = JSON.parse(userlogged);
+    user.innerText = user.innerText + "Usuario Logueado:" + userlogged.email;
+    infouser.style = "display:inline-block";
+  }
 });
--- js/login.js
+++ js/login.js
@@ -1,75 +1,27 @@
-const USUARIOS_URL = "https://eliel-mengassini.github.io/proyect-ecommerce/js/usuarios.json";
+document.addEventListener("DOMContentLoaded", function (e) {
 
-var getJSONData2 = function(url){
-    var result = {};
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        return result;
-    });
-}
-
-var usersArray = [];
-
-function validateUser(usersArray, userIn, passwordIn) { 
-    for (let i = 0; i < usersArray.length; i++) {
-        let usuario = usersArray[i];
-        if (usuario.name == userIn && usuario.password == passwordIn){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    
-} 
-
-document.addEventListener("DOMContentLoaded", function(e){
-
-    document.getElementById("submitBtn").addEventListener("click", function(e) {
+    document.getElementById("submitBtn").addEventListener("click", function (e) {
 
         let inputUser = document.getElementById("inputUser");
         let inputPassword = document.getElementById("inputPassword");
         let camposCompletos = true;
-        
+
         if (inputUser.value === "") {
             inputUser.classList.add("invalid");
             camposCompletos = false;
         }
 
-        if (inputPassword.value === ""){
+        if (inputPassword.value === "") {
             inputPassword.classList.add("invalid");
             camposCompletos = false;
         }
 
         if (camposCompletos) {
+            localStorage.setItem("user-logged", JSON.stringify({ email: inputUser.value }));
+            window.location = "https://eliel-mengassini.github.io/proyect-ecommerce/home.html";
+            
 
-            getJSONData2(USUARIOS_URL).then(function (resultObj) {
-                if (resultObj.status === "ok") {
-                    usersArray = resultObj.data;
-        
-                    if ( validateUser(usersArray, inputUser.value, inputPassword.value) ){
-                        window.location = "https://eliel-mengassini.github.io/proyect-ecommerce/home.html";
-                    }else{
-                        alert("Usuario o contraseña incorrectas!");
-                    }
-                }
-            });
-
-        }else{
+        } else {
             alert("Debes ingresar los datos!")
         }
     });
