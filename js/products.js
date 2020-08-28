68c5820924b242d5d58e5c40543f0c5501849815
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-08-28 11:21:28 -0300

    update12.7

--- js/products.js
+++ js/products.js
@@ -5,6 +5,10 @@ var productArray = [];
 var currentSortCriteria = undefined;
 var minCount = undefined;
 var maxCount = undefined;
+const form = document.querySelector("#formulario")
+const buscadorBoton = document.querySelector("#searchButton");
+const resultado = document.querySelector("#product-container")
+
 
 function sortProducts(criteria, array) {
     let result = [];
@@ -137,3 +141,43 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 });
 
+const filter = () => {
+    resultado.innerHTML = '';
+    const textSearched = form.value.toLowerCase();
+    for (let product of productArray) {
+        let car = product.name.toLowerCase();
+        if (car.indexOf(textSearched) !== -1) {
+            resultado.innerHTML += `
+            <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ product.name + `</h4>
+                        <p>`+ product.description + `</p>
+                        </div>
+                        <small class="text-muted">` + product.cost + ` ` + product.currency + `</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        }
+    }
+
+    if (resultado.innerHTML === '') {
+        resultado.innerHTML += `
+        <div>
+        <p class="text-muted" text-allign="left">No se encuentra el producto buscado             </p>
+        </div>
+        `
+    }
+}
+buscadorBoton.addEventListener('click', filter);
+form.addEventListener('keyup', filter);
+filter();
