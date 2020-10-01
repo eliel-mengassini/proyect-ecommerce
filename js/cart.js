dd25bdc60731f63f19f6a6fdc8cb74c3b7f8d0b4
eliel-mengassini <68951692+eliel-mengassini@users.noreply.github.com>
2020-10-01 12:06:10 -0300

    update 15

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,27 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var cartArray = [];
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            cartArray = resultObj.data;
+            showCartList(cartArray);
+        }
+    });
 });
+
+function showCartList(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+
+        let carrito = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-3">
+        <img src="` + carrito.src + `" class="img-thumbnail">
+        </div>
+
+        `
+    }
+};
